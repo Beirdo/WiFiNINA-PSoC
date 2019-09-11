b0600c2edde63be7df1c7dc949ece023151a0087
Gavin Hurlbut <gjhurlbu@gmail.com>
2019-09-10 21:20:56 -0700

    First pass at converting WiFi.[ch]

--- include/WiFi.h
+++ include/WiFi.h
@@ -24,249 +24,233 @@
 
 #define WIFI_FIRMWARE_LATEST_VERSION "1.2.1"
 
-#include <inttypes.h>
-#include <Arduino.h>
-#include <SPI.h>
+#include "project.h"
 
-extern "C" {
-	#include "utility/wl_definitions.h"
-	#include "utility/wl_types.h"
-}
-
+#include "wl_definitions.h"
+#include "wl_types.h"
 #include "IPAddress.h"
 #include "WiFiClient.h"
 #include "WiFiSSLClient.h"
 #include "WiFiServer.h"
 
-class WiFiClass
-{
-private:
+void WiFi_init();
 
-    static void init();
-public:
-    WiFiClass();
-
-    /*
-     * Get firmware version
-     */
-    static const char* firmwareVersion();
+/*
+ * Get firmware version
+ */
+const char* WiFi_firmwareVersion();
 
 
-    /* Start Wifi connection for OPEN networks
-     *
-     * param ssid: Pointer to the SSID string.
-     */
-    int begin(const char* ssid);
+/* Start Wifi connection for OPEN networks
+ *
+ * param ssid: Pointer to the SSID string.
+ */
+int WiFi_begin_open(const char* ssid);
 
-    /* Start Wifi connection with WEP encryption.
-     * Configure a key into the device. The key type (WEP-40, WEP-104)
-     * is determined by the size of the key (5 bytes for WEP-40, 13 bytes for WEP-104).
-     *
-     * param ssid: Pointer to the SSID string.
-     * param key_idx: The key index to set. Valid values are 0-3.
-     * param key: Key input buffer.
-     */
-    int begin(const char* ssid, uint8_t key_idx, const char* key);
+/* Start Wifi connection with WEP encryption.
+ * Configure a key into the device. The key type (WEP-40, WEP-104)
+ * is determined by the size of the key (5 bytes for WEP-40, 13 bytes for WEP-104).
+ *
+ * param ssid: Pointer to the SSID string.
+ * param key_idx: The key index to set. Valid values are 0-3.
+ * param key: Key input buffer.
+ */
+int WiFi_begin_WEP(const char* ssid, uint8 key_idx, const char* key);
 
-    /* Start Wifi connection with passphrase
-     * the most secure supported mode will be automatically selected
-     *
-     * param ssid: Pointer to the SSID string.
-     * param passphrase: Passphrase. Valid characters in a passphrase
-     *        must be between ASCII 32-126 (decimal).
-     */
-    int begin(const char* ssid, const char *passphrase);
+/* Start Wifi connection with passphrase
+ * the most secure supported mode will be automatically selected
+ *
+ * param ssid: Pointer to the SSID string.
+ * param passphrase: Passphrase. Valid characters in a passphrase
+ *        must be between ASCII 32-126 (decimal).
+ */
+int WiFi_begin_passphrase(const char* ssid, const char *passphrase);
 
-    uint8_t beginAP(const char *ssid);
-    uint8_t beginAP(const char *ssid, uint8_t channel);
-    uint8_t beginAP(const char *ssid, const char* passphrase);
-    uint8_t beginAP(const char *ssid, const char* passphrase, uint8_t channel);
+uint8_t WiFi_beginAP_ssid(const char *ssid);
+uint8_t WiFi_beginAP_ssid_channel(const char *ssid, uint8 channel);
+uint8_t WiFi_beginAP_ssid_passphrase(const char *ssid, const char* passphrase);
+uint8_t WiFi_beginAP_ssid_passphrase_channel(const char *ssid, const char* passphrase, uint8 channel);
 
-    /* Change Ip configuration settings disabling the dhcp client
-        *
-        * param local_ip: 	Static ip configuration
-        */
-    void config(IPAddress local_ip);
+/* Change Ip configuration settings disabling the dhcp client
+    *
+    * param local_ip: 	Static ip configuration
+    */
+void WiFi_config_static(IPAddress local_ip);
 
-    /* Change Ip configuration settings disabling the dhcp client
-        *
-        * param local_ip: 	Static ip configuration
-	* param dns_server:     IP configuration for DNS server 1
-        */
-    void config(IPAddress local_ip, IPAddress dns_server);
+/* Change Ip configuration settings disabling the dhcp client
+    *
+    * param local_ip: 	Static ip configuration
+* param dns_server:     IP configuration for DNS server 1
+    */
+void WiFi_config_static_dns(IPAddress local_ip, IPAddress dns_server);
 
-    /* Change Ip configuration settings disabling the dhcp client
-        *
-        * param local_ip: 	Static ip configuration
-	* param dns_server:     IP configuration for DNS server 1
-        * param gateway : 	Static gateway configuration
-        */
-    void config(IPAddress local_ip, IPAddress dns_server, IPAddress gateway);
+/* Change Ip configuration settings disabling the dhcp client
+    *
+    * param local_ip: 	Static ip configuration
+* param dns_server:     IP configuration for DNS server 1
+    * param gateway : 	Static gateway configuration
+    */
+void WiFi_config_static_dns_gateway(IPAddress local_ip, IPAddress dns_server, IPAddress gateway);
 
-    /* Change Ip configuration settings disabling the dhcp client
-        *
-        * param local_ip: 	Static ip configuration
-	* param dns_server:     IP configuration for DNS server 1
-        * param gateway: 	Static gateway configuration
-        * param subnet:		Static Subnet mask
-        */
-    void config(IPAddress local_ip, IPAddress dns_server, IPAddress gateway, IPAddress subnet);
+/* Change Ip configuration settings disabling the dhcp client
+    *
+    * param local_ip: 	Static ip configuration
+* param dns_server:     IP configuration for DNS server 1
+    * param gateway: 	Static gateway configuration
+    * param subnet:		Static Subnet mask
+    */
+void WiFi_config_static_dns_gateway_subnet(IPAddress local_ip, IPAddress dns_server, IPAddress gateway, IPAddress subnet);
 
-    /* Change DNS Ip configuration
-     *
-     * param dns_server1: ip configuration for DNS server 1
-     */
-    void setDNS(IPAddress dns_server1);
+/* Change DNS Ip configuration
+ *
+ * param dns_server1: ip configuration for DNS server 1
+ */
+void WiFi_setDNS(IPAddress dns_server1);
 
-    /* Change DNS Ip configuration
-     *
-     * param dns_server1: ip configuration for DNS server 1
-     * param dns_server2: ip configuration for DNS server 2
-     *
-     */
-    void setDNS(IPAddress dns_server1, IPAddress dns_server2);
+/* Change DNS Ip configuration
+ *
+ * param dns_server1: ip configuration for DNS server 1
+ * param dns_server2: ip configuration for DNS server 2
+ *
+ */
+void WiFi_setDNS_two_servers(IPAddress dns_server1, IPAddress dns_server2);
 
 
-    /* Set the hostname used for DHCP requests
-     *
-     * param name: hostname to set
-     *
-     */
-    void setHostname(const char* name);
+/* Set the hostname used for DHCP requests
+ *
+ * param name: hostname to set
+ *
+ */
+void WiFi_setHostname(const char* name);
 
-    /*
-     * Disconnect from the network
-     *
-     * return: one value of wl_status_t enum
-     */
-    int disconnect(void);
+/*
+ * Disconnect from the network
+ *
+ * return: one value of wl_status_t enum
+ */
+int WiFi_disconnect(void);
 
-    void end(void);
+void WiFi_end(void);
 
-    /*
-     * Get the interface MAC address.
-     *
-     * return: pointer to uint8_t array with length WL_MAC_ADDR_LENGTH
-     */
-    uint8_t* macAddress(uint8_t* mac);
+/*
+ * Get the interface MAC address.
+ *
+ * return: pointer to uint8_t array with length WL_MAC_ADDR_LENGTH
+ */
+uint8* WiFi_macAddress(uint8* mac);
 
-    /*
-     * Get the interface IP address.
-     *
-     * return: Ip address value
-     */
-    IPAddress localIP();
+/*
+ * Get the interface IP address.
+ *
+ * return: Ip address value
+ */
+IPAddress WiFi_localIP();
 
-    /*
-     * Get the interface subnet mask address.
-     *
-     * return: subnet mask address value
-     */
-    IPAddress subnetMask();
+/*
+ * Get the interface subnet mask address.
+ *
+ * return: subnet mask address value
+ */
+IPAddress WiFi_subnetMask();
 
-    /*
-     * Get the gateway ip address.
-     *
-     * return: gateway ip address value
-     */
-   IPAddress gatewayIP();
+/*
+ * Get the gateway ip address.
+ *
+ * return: gateway ip address value
+ */
+IPAddress WiFi_gatewayIP();
 
-    /*
-     * Return the current SSID associated with the network
-     *
-     * return: ssid string
-     */
-    const char* SSID();
+/*
+ * Return the current SSID associated with the network
+ *
+ * return: ssid string
+ */
+const char* WiFi_SSID();
 
-    /*
-      * Return the current BSSID associated with the network.
-      * It is the MAC address of the Access Point
-      *
-      * return: pointer to uint8_t array with length WL_MAC_ADDR_LENGTH
-      */
-    uint8_t* BSSID(uint8_t* bssid);
+/*
+  * Return the current BSSID associated with the network.
+  * It is the MAC address of the Access Point
+  *
+  * return: pointer to uint8_t array with length WL_MAC_ADDR_LENGTH
+  */
+uint8* WiFi_BSSID(uint8* bssid);
 
-    /*
-      * Return the current RSSI /Received Signal Strength in dBm)
-      * associated with the network
-      *
-      * return: signed value
-      */
-    int32_t RSSI();
+/*
+  * Return the current RSSI /Received Signal Strength in dBm)
+  * associated with the network
+  *
+  * return: signed value
+  */
+int32 WiFi_RSSI();
 
-    /*
-      * Return the Encryption Type associated with the network
-      *
-      * return: one value of wl_enc_type enum
-      */
-    uint8_t	encryptionType();
+/*
+  * Return the Encryption Type associated with the network
+  *
+  * return: one value of wl_enc_type enum
+  */
+uint8	WiFi_encryptionType();
 
-    /*
-     * Start scan WiFi networks available
-     *
-     * return: Number of discovered networks
-     */
-    int8_t scanNetworks();
+/*
+ * Start scan WiFi networks available
+ *
+ * return: Number of discovered networks
+ */
+int8 WiFi_scanNetworks();
 
-    /*
-     * Return the SSID discovered during the network scan.
-     *
-     * param networkItem: specify from which network item want to get the information
-	 *
-     * return: ssid string of the specified item on the networks scanned list
-     */
-    const char*	SSID(uint8_t networkItem);
+/*
+ * Return the SSID discovered during the network scan.
+ *
+ * param networkItem: specify from which network item want to get the information
+ *
+ * return: ssid string of the specified item on the networks scanned list
+ */
+const char*	WiFi_SSID_index(uint8 networkItem);
 
-    /*
-     * Return the encryption type of the networks discovered during the scanNetworks
-     *
-     * param networkItem: specify from which network item want to get the information
-	 *
-     * return: encryption type (enum wl_enc_type) of the specified item on the networks scanned list
-     */
-    uint8_t	encryptionType(uint8_t networkItem);
+/*
+ * Return the encryption type of the networks discovered during the scanNetworks
+ *
+ * param networkItem: specify from which network item want to get the information
+ *
+ * return: encryption type (enum wl_enc_type) of the specified item on the networks scanned list
+ */
+uint8	WiFi_encryptionType_index(uint8 networkItem);
 
-    uint8_t* BSSID(uint8_t networkItem, uint8_t* bssid);
-    uint8_t channel(uint8_t networkItem);
+uint8* WiFi_BSSID_index(uint8 networkItem, uint8* bssid);
+uint8 WiFi_channel_index(uint8 networkItem);
 
-    /*
-     * Return the RSSI of the networks discovered during the scanNetworks
-     *
-     * param networkItem: specify from which network item want to get the information
-	 *
-     * return: signed value of RSSI of the specified item on the networks scanned list
-     */
-    int32_t RSSI(uint8_t networkItem);
+/*
+ * Return the RSSI of the networks discovered during the scanNetworks
+ *
+ * param networkItem: specify from which network item want to get the information
+ *
+ * return: signed value of RSSI of the specified item on the networks scanned list
+ */
+int32 WiFi_RSSI_index(uint8 networkItem);
 
-    /*
-     * Return Connection status.
-     *
-     * return: one of the value defined in wl_status_t
-     */
-    uint8_t status();
+/*
+ * Return Connection status.
+ *
+ * return: one of the value defined in wl_status_t
+ */
+uint8_t WiFi_status();
 
-    /*
-     * Resolve the given hostname to an IP address.
-     * param aHostname: Name to be resolved
-     * param aResult: IPAddress structure to store the returned IP address
-     * result: 1 if aIPAddrString was successfully converted to an IP address,
-     *          else error code
-     */
-    int hostByName(const char* aHostname, IPAddress& aResult);
+/*
+ * Resolve the given hostname to an IP address.
+ * param aHostname: Name to be resolved
+ * param aResult: pointer to IPAddress structure to store the returned IP address
+ * result: 1 if aIPAddrString was successfully converted to an IP address,
+ *          else error code
+ */
+int WiFi_hostByName(const char* aHostname, IPAddress *aResult);
 
-    unsigned long getTime();
+unsigned long WiFi_getTime();
 
-    void lowPowerMode();
-    void noLowPowerMode();
+void WiFi_lowPowerMode();
+void WiFi_noLowPowerMode();
 
-    int ping(const char* hostname, uint8_t ttl = 128);
-    int ping(const String &hostname, uint8_t ttl = 128);
-    int ping(IPAddress host, uint8_t ttl = 128);
+int WiFi_ping_hostname(const char* hostname, uint8 ttl);
+int WiFi_ping_ipaddress(IPAddress host, uint8 ttl);
 
-    void setPins(int8_t cs=10, int8_t ready=7, int8_t reset=5, int8_t gpio0=6, SPIClass *spi = &SPI);
-    void setLEDs(uint8_t red, uint8_t green, uint8_t blue);
-};
-
-extern WiFiClass WiFi;
+void WiFi_setLEDs(uint8 red, uint8 green, uint8 blue);
 
 #endif
---
+++ src/WiFi.c
@@ -0,0 +1,274 @@
+/*
+  WiFi.c - Library for Arduino Wifi shield ported to C.
+  Copyright (c) 2019 Gavin Hurlbut.  All rights reserved.
+  Copyright (c) 2018 Arduino SA. All rights reserved.
+  Copyright (c) 2011-2014 Arduino LLC.  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "project.h"
+#include "wifi_drv.h"
+#include "WiFi.h"
+
+extern SPIClass *WIFININA_SPIWIFI;
+extern int8 WIFININA_SLAVESELECT, WIFININA_SLAVEREADY, WIFININA_SLAVERESET, WIFININA_SLAVEGPIO0;
+
+#include "wl_definitions.h"
+#include "wl_types.h"
+#include "debug.h"
+
+
+void WiFi_setLEDs(uint8 red, uint8 green, uint8 blue) {
+    WiFiDrv_pinMode(25, OUTPUT);
+    WiFiDrv_pinMode(26, OUTPUT);
+    WiFiDrv_pinMode(27, OUTPUT);
+    WiFiDrv_analogWrite(25, red);
+    WiFiDrv_analogWrite(26, green);
+    WiFiDrv_analogWrite(27, blue);
+}
+
+void WiFi_init() {
+    WiFiDrv_wifiDriverInit();
+}
+
+const char *WiFi_firmwareVersion() {
+    return WiFiDrv_getFwVersion();
+}
+
+int WiFi_begin_common(void) {
+    uint8 status = WL_IDLE_STATUS;
+    uint8 attempts = WL_MAX_ATTEMPT_CONNECTION;
+
+    do {
+        delay(WL_DELAY_START_CONNECTION);
+        status = WiFiDrv_getConnectionStatus();
+    } while (((status == WL_IDLE_STATUS) || (status == WL_NO_SSID_AVAIL) || (status == WL_SCAN_COMPLETED)) &&
+             (--attempts > 0));
+    return status;
+
+}
+
+int WiFi_begin_open(const char *ssid) {
+    if (WiFiDrv_wifiSetNetwork(ssid, strlen(ssid)) == WL_FAILURE) {
+        return WL_CONNECT_FAILED;
+    }
+    return WiFi_begin_common();
+}
+
+int WiFi_begin_WEP(const char *ssid, uint8 key_idx, const char *key) {
+    // set encryption key
+    if (WiFiDrv_wifiSetKey(ssid, strlen(ssid), key_idx, key, strlen(key)) == WL_FAILURE) {
+        status = WL_CONNECT_FAILED;
+    }
+    return WiFi_begin_common();
+}
+
+int WiFi_begin(const char *ssid, const char *passphrase) {
+    // set passphrase
+    if (WiFiDrv_wifiSetPassphrase(ssid, strlen(ssid), passphrase, strlen(passphrase)) == WL_FAILURE) {
+        status = WL_CONNECT_FAILED;
+    }
+    return WiFi_begin_common();
+}
+
+uint8 WiFi_beginAP_common(void)
+{
+    uint8 status = WL_IDLE_STATUS;
+    uint8 attempts = WL_MAX_ATTEMPT_CONNECTION;
+
+    do {
+        delay(WL_DELAY_START_CONNECTION);
+        status = WiFiDrv_getConnectionStatus();
+    } while (((status == WL_IDLE_STATUS) || (status == WL_SCAN_COMPLETED)) && (--attempts > 0));
+    return status;
+}
+
+uint8 WiFi_beginAP_ssid(const char *ssid) {
+    return WiFi_beginAP_ssid_channel(ssid, 1);
+}
+
+uint8 WiFi_beginAP_ssid_channel(const char *ssid, uint8 channel) {
+    if (WiFiDrv_wifiSetApNetwork(ssid, strlen(ssid), channel) == WL_FAILURE) {
+        return WL_AP_FAILED;
+    }
+    return WiFi_beginAP_common();
+}
+
+uint8 WiFi_beginAP_ssid_channel(const char *ssid, const char *passphrase) {
+    return WiFi_beginAP_ssid_passphrase_channel(ssid, passphrase, 1);
+}
+
+uint8 WiFi_beginAP_ssid_passphrase_channel(const char *ssid, const char *passphrase, uint8 channel) {
+    // set passphrase
+    if (WiFiDrv_wifiSetApPassphrase(ssid, strlen(ssid), passphrase, strlen(passphrase), channel) == WL_FAILURE) {
+        return WL_AP_FAILED;
+    }
+    return WiFi_beginAP_common();
+}
+
+void WiFi_config_static(IPAddress local_ip) {
+    WiFiDrv_config(1, (uint32) local_ip, 0, 0);
+}
+
+void WiFi_config_static_dns(IPAddress local_ip, IPAddress dns_server) {
+    WiFiDrv_config(1, (uint32) local_ip, 0, 0);
+    WiFiDrv_setDNS(1, (uint32) dns_server, 0);
+}
+
+void WiFi_config_static_dns_gateway(IPAddress local_ip, IPAddress dns_server, IPAddress gateway) {
+    WiFiDrv_config(2, (uint32) local_ip, (uint32) gateway, 0);
+    WiFiDrv_setDNS(1, (uint32) dns_server, 0);
+}
+
+void WiFi_config_static_dns_gateway_subnet(IPAddress local_ip, IPAddress dns_server, IPAddress gateway, IPAddress subnet) {
+    WiFiDrv_config(3, (uint32) local_ip, (uint32) gateway, (uint32) subnet);
+    WiFiDrv_setDNS(1, (uint32) dns_server, 0);
+}
+
+void WiFi_setDNS(IPAddress dns_server1) {
+    WiFiDrv_setDNS(1, (uint32) dns_server1, 0);
+}
+
+void WiFi_setDNS_two_servers(IPAddress dns_server1, IPAddress dns_server2) {
+    WiFiDrv_setDNS(2, (uint32) dns_server1, (uint32) dns_server2);
+}
+
+void WiFi_setHostname(const char *name) {
+    WiFiDrv_setHostname(name);
+}
+
+int WiFi_disconnect() {
+    return WiFiDrv_disconnect();
+}
+
+void WiFi_end(void) {
+    WiFiDrv_wifiDriverDeinit();
+}
+
+uint8 *WiFi_macAddress(uint8 *mac) {
+    uint8 *_mac = WiFiDrv_getMacAddress();
+    memcpy(mac, _mac, WL_MAC_ADDR_LENGTH);
+    return mac;
+}
+
+IPAddress WiFi_localIP() {
+    IPAddress ret;
+    WiFiDrv_getIpAddress(ret);
+    return ret;
+}
+
+IPAddress WiFi_subnetMask() {
+    IPAddress ret;
+    WiFiDrv_getSubnetMask(ret);
+    return ret;
+}
+
+IPAddress WiFi_gatewayIP() {
+    IPAddress ret;
+    WiFiDrv_getGatewayIP(ret);
+    return ret;
+}
+
+const char *WiFi_SSID() {
+    return WiFiDrv_getCurrentSSID();
+}
+
+uint8 *WiFi_BSSID(uint8 *bssid) {
+    uint8 *_bssid = WiFiDrv_getCurrentBSSID();
+    memcpy(bssid, _bssid, WL_MAC_ADDR_LENGTH);
+    return bssid;
+}
+
+int32 WiFi_RSSI() {
+    return WiFiDrv_getCurrentRSSI();
+}
+
+uint8 WiFi_encryptionType() {
+    return WiFiDrv_getCurrentEncryptionType();
+}
+
+
+int8 WiFi_scanNetworks() {
+    uint8 attempts = 10;
+    uint8 numOfNetworks = 0;
+
+    if (WiFiDrv_startScanNetworks() == WL_FAILURE) {
+        return WL_FAILURE;
+    }
+
+    do {
+        delay(2000);
+        numOfNetworks = WiFiDrv_getScanNetworks();
+    } while ((numOfNetworks == 0) && (--attempts > 0));
+    return numOfNetworks;
+}
+
+const char *WiFi_SSID_index(uint8 networkItem) {
+    return WiFiDrv_getSSIDNetworks(networkItem);
+}
+
+int32 WiFi_RSSI_index(uint8 networkItem) {
+    return WiFiDrv_getRSSINetworks(networkItem);
+}
+
+uint8 WiFi_encryptionType_index(uint8 networkItem) {
+    return WiFiDrv_getEncTypeNetworks(networkItem);
+}
+
+uint8 *WiFi_BSSID_index(uint8 networkItem, uint8 *bssid) {
+    return WiFiDrv_getBSSIDNetworks(networkItem, bssid);
+}
+
+uint8 WiFi_channelIndex(uint8 networkItem) {
+    return WiFiDrv_getChannelNetworks(networkItem);
+}
+
+uint8 WiFi_status() {
+    return WiFiDrv_getConnectionStatus();
+}
+
+int WiFi_hostByName(const char *aHostname, IPAddress *aResult) {
+    return WiFiDrv_getHostByName(aHostname, aResult);
+}
+
+unsigned long WiFi_getTime() {
+    return WiFiDrv_getTime();
+}
+
+void WiFi_lowPowerMode() {
+    WiFiDrv_setPowerMode(1);
+}
+
+void WiFi_noLowPowerMode() {
+    WiFiDrv_setPowerMode(0);
+}
+
+int WiFi_ping_hostname(const char *hostname, int ttl) {
+    IPAddress ip;
+
+    if (!hostByName(hostname, ip)) {
+        return WL_PING_UNKNOWN_HOST;
+    }
+
+    return WiFi_ping_ipaddress(ip, ttl);
+}
+
+int WiFi_ping_ipaddress(IPAddress host, int ttl) {
+    if(ttl <= 0) {
+        ttl = 128;
+    }
+    return WiFiDrv_ping(host, ttl);
+}
